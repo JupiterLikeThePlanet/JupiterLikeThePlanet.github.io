e6c54ef88fcc50c9b2031e1f8ea7765e28045691
jupiterliketheplanet <jupiterbaudot@gmail.com>
2015-10-18 12:06:24 -0700

    Modify blog posts

---
+++ jquery/blog-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	// var $img = $('img')
+	
+	$('figure').hover(function(){
+		$('.row1').addClass('highlight');
+		// (this).css('background-color','#00A693');
+		// fadeIn('fast', 0.5)
+
+	}};
+
+});
